57f3d67caa2087efe5919661fe24c6ab6b2e0e94
captaincyber21 <sartaj.singh3@education.nsw.gov.au>
2025-06-17 13:12:31 +1000

    v1.3 preview
    API update to support the biome and armor registry and utils

--- src/main/java/com/noctify/API/RegistryAPI.java
+++ src/main/java/com/noctify/API/RegistryAPI.java
@@ -1,61 +1,85 @@
+// src/main/java/com/noctify/API/RegistryAPI.java
 package com.noctify.API;
 
 import com.noctify.Custom.ItemRegistry;
+import com.noctify.Custom.ArmorRegistry;
 import com.noctify.Custom.CommandRegistry;
 import com.noctify.Custom.EventRegistry;
 import com.noctify.Custom.FoodRegistry;
+import com.noctify.Custom.BiomeRegistry;
 import com.noctify.Main.Registration.FXFoodBehavior;
 import com.noctify.Main.Registration.FXFoodDefinition;
 import com.noctify.Main.Registration.FXItemDefinition;
+import com.noctify.Main.Registration.FXArmorDefinition;
+import com.noctify.Main.Registration.FXBiomeDefinition;
 import com.noctify.Main.Registration.PermissionLevel;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.Location;
 
-public class RegistryAPI implements Registry {
-    @Override
-    public void registerItem(String itemName, FXItemDefinition itemDefinition) {
+public class RegistryAPI {
+
+    // Item registry
+    public static void registerItem(String itemName, FXItemDefinition itemDefinition) {
         ItemRegistry.registerItem(itemName, itemDefinition);
     }
 
-    @Override
-    public ItemStack getCustomItem(String itemName) {
+    public static ItemStack getCustomItem(String itemName) {
         return ItemRegistry.getCustomItem(itemName);
     }
 
-    @Override
-    public void registerCommand(Plugin plugin, String commandName, Class<? extends CommandExecutor> commandClass) {
+    // Armor registry
+    public static void registerArmor(String armorName, FXArmorDefinition armorDefinition) {
+        ArmorRegistry.registerArmor(armorName, armorDefinition);
+    }
+
+    public static ItemStack getCustomArmor(String armorName) {
+        return ArmorRegistry.getCustomArmor(armorName);
+    }
+
+    // Command registry
+    public static void registerCommand(Plugin plugin, String commandName, Class<? extends CommandExecutor> commandClass) {
         CommandRegistry.registerCommand(plugin, commandName, commandClass);
     }
 
-    // New: Register command with explicit permission level and class
-    public void registerCommand(Plugin plugin, String commandName, Class<? extends CommandExecutor> commandClass, PermissionLevel requiredLevel) {
+    public static void registerCommand(Plugin plugin, String commandName, Class<? extends CommandExecutor> commandClass, PermissionLevel requiredLevel) {
         CommandRegistry.registerCommand(plugin, commandName, commandClass, requiredLevel);
     }
 
-    @Override
-    public void registerCommand(Plugin plugin, String commandName) {
+    public static void registerCommand(Plugin plugin, String commandName) {
         CommandRegistry.registerCommand(plugin, commandName, PermissionLevel.PLAYER);
     }
 
-    // New: Register command with explicit permission level (by name)
-    public void registerCommand(Plugin plugin, String commandName, PermissionLevel requiredLevel) {
+    public static void registerCommand(Plugin plugin, String commandName, PermissionLevel requiredLevel) {
         CommandRegistry.registerCommand(plugin, commandName, requiredLevel);
     }
 
-    @Override
-    public void registerEvent(Plugin plugin, Class<? extends Listener> eventClass) {
+    // Event registry
+    public static void registerEvent(Plugin plugin, Class<? extends Listener> eventClass) {
         EventRegistry.registerEvent(plugin, eventClass);
     }
 
-    @Override
-    public void registerEvent(Plugin plugin, String eventName) {
+    public static void registerEvent(Plugin plugin, String eventName) {
         EventRegistry.registerEvent(plugin, eventName);
     }
 
-    @Override
-    public void registerFood(String foodName, FXFoodDefinition definition, FXFoodBehavior behavior) {
+    // Food registry
+    public static void registerFood(String foodName, FXFoodDefinition definition, FXFoodBehavior behavior) {
         FoodRegistry.registerFood(foodName, definition, behavior);
+    }
+
+    // Biome registry
+    public static void registerBiome(String biomeName, FXBiomeDefinition biomeDefinition) {
+        BiomeRegistry.registerBiome(biomeName, biomeDefinition);
+    }
+
+    public static FXBiomeDefinition getBiomeDefinition(String biomeName) {
+        return BiomeRegistry.getBiomeDefinition(biomeName);
+    }
+
+    public static FXBiomeDefinition getBiomeDefinitionAt(Location location) {
+        return BiomeRegistry.getBiomeDefinitionAt(location);
     }
 }
--- src/main/java/com/noctify/API/UtilsAPI.java
+++ src/main/java/com/noctify/API/UtilsAPI.java
@@ -23,94 +23,76 @@ import org.bukkit.entity.EntityType;
 import java.util.Map;
 import java.util.UUID;
 
-public class UtilsAPI implements Utils {
+public class UtilsAPI {
 
-    @Override
     public boolean isOnCooldown(UUID playerId, String key) {
         return CooldownUtils.isOnCooldown(playerId, key);
     }
 
-    @Override
     public double getRemainingCooldown(UUID playerId, String key) {
         return CooldownUtils.getRemainingCooldown(playerId, key);
     }
 
-    @Override
     public void setCooldown(UUID playerId, String key, int seconds) {
         CooldownUtils.setCooldown(playerId, key, seconds);
     }
 
-    @Override
     public void sendCooldownMessage(Player player, String itemName, double timeLeft) {
         CooldownUtils.sendCooldownMessage(player, itemName, timeLeft);
     }
 
-    @Override
     public void clearAllCooldowns() {
         CooldownUtils.clearAllCooldowns();
     }
 
-    @Override
     public boolean isCustomItem(ItemStack item, Material material, String displayName) {
         return CustomItemUtils.isCustomItem(item, material, displayName);
     }
 
-    @Override
     public void addEffectWhileHolding(Player player, Material itemMaterial, PotionEffectType effectType, int duration, int amplifier) {
         EffectUtils.addEffectWhileHolding(player, itemMaterial, effectType, duration, amplifier);
     }
 
-    @Override
     public void removeEffectTask(Player player) {
         EffectUtils.removeEffectTask(player);
     }
 
-    @Override
     public void setSize(Entity entity, float scale) {
         EntitySizeUtils.setSize(entity, scale);
     }
 
-    @Override
     public void resetSize(Entity entity) {
         EntitySizeUtils.resetSize(entity);
     }
 
-    @Override
     public void initializeManaUtils(Plugin plugin) {
         ManaUtils.initialize(plugin);
     }
 
-    @Override
     public boolean useMana(Player player, int amount) {
         return ManaUtils.useMana(player, amount);
     }
 
-    @Override
     public void setMana(Player player, int amount) {
         ManaUtils.setMana(player, amount);
     }
 
-    @Override
     public int getMana(Player player) {
         return ManaUtils.getMana(player);
     }
 
-    @Override
     public void registerOneTimeCraft(OneTimeCraftUtils instance, String name, ItemStack item) {
         instance.registerCraft(name, item);
     }
 
-    @Override
     public Map<String, OneTimeCraftUtils.OneTimeCraftItem> getRegisteredOneTimeCraftItems() {
         return OneTimeCraftUtils.getRegisteredItems();
     }
 
-    @Override
     public void initializeProjectileUtils(Plugin plugin) {
         ProjectileUtils.initialize(plugin);
     }
 
-    @Override
     public void createCustomProjectile(
             Plugin plugin,
             Player shooter,
@@ -139,7 +121,6 @@ public class UtilsAPI implements Utils {
         }
     }
 
-    @Override
     public boolean teleportPlayer(Player player, Location targetLocation, int maxDistance, boolean preserveDirection, boolean playSound, boolean spawnParticles) {
         return TeleportUtils.teleportPlayer(player, targetLocation, maxDistance, preserveDirection, playSound, spawnParticles);
     }
